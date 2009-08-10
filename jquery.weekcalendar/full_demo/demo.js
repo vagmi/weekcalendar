62fb5cbadf70c011db5cc8fec398dfdd8431e813
robmonie <robmonie@f3a9831a-37e4-11de-b310-9106b1f7c239>
2009-08-10 00:36:59 +0000

    fixed defect in full demo

--- jquery.weekcalendar/full_demo/demo.js
+++ jquery.weekcalendar/full_demo/demo.js
@@ -152,7 +152,7 @@ $(document).ready(function() {
                {"id":3, "start": new Date(year, month, day + 1, 17), "end": new Date(year, month, day + 1, 17, 45),"title":"Hair cut"},
                {"id":4, "start": new Date(year, month, day - 1, 8), "end": new Date(year, month, day - 1, 9, 30),"title":"Team breakfast"},
                {"id":5, "start": new Date(year, month, day + 1, 14), "end": new Date(year, month, day + 1, 15),"title":"Product showcase"},
-               {"id":5, "start": new Date(year, month, day, 10), "end": new Date(year, month, day, 11),"title":"I'm read-only", readOnly : true}
+               {"id":6, "start": new Date(year, month, day, 10), "end": new Date(year, month, day, 11),"title":"I'm read-only", readOnly : true}
             
             ]
         };
